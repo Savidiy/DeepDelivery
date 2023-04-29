fc7940d49a291d9839a1f65d87d956077c668edc
Leonid Vasilev <vasilev.l@panoramik.ru>
2023-04-29 20:09:20 +0300

    fix after merge

--- src/DeepDelivery/Assets/Scripts/LevelWindowModule/Model/LevelModel.cs
+++ src/DeepDelivery/Assets/Scripts/LevelWindowModule/Model/LevelModel.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
 using MvvmModule;
+using SettingsModule;
+using UnityEngine;
 
 namespace LevelWindowModule
 {
     public sealed class LevelModel : DisposableCollector
     {
+        private readonly LevelBehaviour _levelBehaviour;
+        private readonly List<Enemy> _enemies;
+
+        public LevelModel(LevelBehaviour levelBehaviour, List<Enemy> enemies)
+        {
+            _levelBehaviour = levelBehaviour;
+            _enemies = enemies;
+        }
+
+        public Vector3 GetPlayerStartPosition()
+        {
+            return _levelBehaviour.StartPoint.position;
+        }
     }
 }
--- src/DeepDelivery/Assets/Scripts/LevelWindowModule/Model/LevelModelFactory.cs
+++ src/DeepDelivery/Assets/Scripts/LevelWindowModule/Model/LevelModelFactory.cs
@@ -1,21 +1,39 @@
-using Savidiy.Utils;
+using System.Collections.Generic;
 using SettingsModule;
+using Object = UnityEngine.Object;
 
 namespace LevelWindowModule
 {
     public sealed class LevelModelFactory
     {
-        private readonly TickInvoker _tickInvoker;
+        private readonly EnemyFactory _enemyFactory;
 
-        public LevelModelFactory(TickInvoker tickInvoker)
+        public LevelModelFactory(EnemyFactory enemyFactory)
         {
-            _tickInvoker = tickInvoker;
+            _enemyFactory = enemyFactory;
         }
-
+        
         public LevelModel Create(LevelData levelData)
         {
-            var levelModel = new LevelModel();
+            LevelBehaviour levelBehaviour = Object.Instantiate(levelData.LevelBehaviour);
+
+            List<Enemy> enemies = CreateEnemies(levelBehaviour.EnemySpawnPoints);
+
+            var levelModel = new LevelModel(levelBehaviour, enemies);
             return levelModel;
+        }
+
+        private List<Enemy> CreateEnemies(List<EnemySpawnPointBehaviour> enemySpawnPoints)
+        {
+            var enemies = new List<Enemy>();
+            
+            foreach (EnemySpawnPointBehaviour enemySpawnPoint in enemySpawnPoints)
+            {
+                Enemy enemy = _enemyFactory.CreateEnemy(enemySpawnPoint);
+                enemies.Add(enemy);
+            }
+
+            return enemies;
         }
     }
 }
